4dc0a039dabae7a14e31cf99683c2a56c583ee7f
cjblomqvist <carl@cjblomqvist.com>
2015-01-23 14:58:51 +0100

    Add actual code to navigation component

--- navigation/index.js
+++ navigation/index.js
@@ -6,18 +6,18 @@ Navigation.prototype.view = __dirname + '/views';
 Navigation.prototype.style = __dirname + '/styles';
 Navigation.prototype.name = 'navigation';
 
-// Require in subcomponents
-
 // Require in all actions
 require('./actions');
 
 // Init and create functions
 Navigation.prototype.init = function(model) {
+  var root = model.root;
+  var Navigation = root.Navigation;
 
+  Navigation.getAll();
+  model.ref('items', Navigation.standardSort());
 };
 
-Navigation.prototype.create = function(model, dom) {
-
-};
+Navigation.prototype.create = function(model, dom) {};
 
 // Operations
