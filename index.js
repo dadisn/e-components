120b37a572cabdc88ebda2577260ee2b09eaa7df
Daði Snær Skúlason <dadisn@gmail.com>
2015-01-28 22:06:32 +0100

    Added toast component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function(app, options) {
   // Set up all components
   app.component(null, require('./navigation'), options.ns);
+  app.component(null, require('./toast'), options.ns);
 };
---
+++ toast/index.js
@@ -0,0 +1,65 @@
+var derby = require('derby');
+var Model = derby.Model;
+
+module.exports = Toast;
+
+function Toast() {}
+Toast.prototype.view = __dirname + '/views';
+Toast.prototype.name = 'toast';
+
+var defaultOptions = {
+    sticky: true
+  , timeout: 5000
+};
+
+Model.prototype.toast = function(type, message, options) {
+  // Allow calling with: Toast.add({error: 'Shit just hit the fan'}, options);
+  if(typeof type === 'object') {
+    options = message;
+    for(var key in type) {
+      message = type[key];
+      type = key;
+    }
+  }
+
+  // Change type 'error' to 'danger', and everything unknown to 'info'
+  if(type === 'error') type = 'danger';
+  if(['danger', 'warning', 'info', 'success'].indexOf(type) === -1) type = 'info';
+
+  var self = this;
+  var sticky = options ? options.sticky : defaultOptions.sticky;
+  var timeout = options ? options.timeout : defaultOptions.timeout; 
+  var toast = {
+      id: this.id()
+    , type: type
+    , message: message
+    , sticky: sticky
+  };
+
+  if(options && options.click) toast.click = options.click;
+
+  this.root.unshift('_session.toast', toast, function() {
+    if(sticky) return;
+    setTimeout(function() {
+      var toasts = self.model.root.get('_session.toast');
+      var len = toasts.length;
+
+      for(var i = 0; i < len; i++) {
+        if(toasts[i].id === toast.id) return self.remove(i);
+      }
+    }, timeout);
+  });
+};
+
+Toast.prototype.add = function(type, message, options) {
+  this.model.root.toast(type, message, options);
+};
+
+Toast.prototype.remove = function(index) {
+  var self = this;
+
+  this.model.root.set('_session.toast.' + index + '.faded', true);
+  setTimeout(function() {
+    self.model.root.remove('_session.toast', index);
+  }, 150);
+};
