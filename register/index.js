7b7de3ff866ca7db6fd0bd835ef0f1e18599ff36
Daði Snær Skúlason <dadisn@gmail.com>
2016-10-14 00:37:06 +0000

    Bugfix, route wasn't registering

--- register/index.js
+++ register/index.js
@@ -6,3 +6,4 @@ Register.prototype = require('derby-login/components/base').prototype;
 Register.prototype.view = __dirname + '/views/';
 Register.prototype.name = 'auth:register';
 Register.prototype.fields = ['email', 'password', 'confirm', 'groupId', 'isAdd'];
+Register.prototype.route = 'register';
